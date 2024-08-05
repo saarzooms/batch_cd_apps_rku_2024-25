c56293a800faf822d7ea675a23591ec5e64209d0
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-08-05 11:03:08 +0530

    update to navigate to login screen with design updates

--- ecom_app/lib/screen/splash_screen.dart
+++ ecom_app/lib/screen/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:ecom_app/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -9,21 +12,38 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: [
-      Text(
-        'My Ecom App',
-        style: TextStyle(
-          fontSize: 40,
+      body: Column(children: [
+        Expanded(
+          child: Center(
+            child: Text(
+              'My Ecom App',
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
         ),
-      ),
-      Text(
-        'Developed By : RKU Students',
-        style: TextStyle(
-          fontSize: 20,
+        Text(
+          'Developed By : RKU Students',
+          style: TextStyle(
+            fontSize: 20,
+          ),
         ),
-      ),
-    ]));
+      ]),
+    );
   }
 }
