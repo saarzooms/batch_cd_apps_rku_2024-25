98b8376d231846167606cdeac2977d318aed39fc
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-08-05 11:41:42 +0530

    to display user on screen + bottom navigation bar

--- ecom_app/lib/screen/dashboard_screen.dart
+++ ecom_app/lib/screen/dashboard_screen.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  String unm = '';
+  @override
+  void initState() {
+    super.initState();
+    fetchUser();
+  }
+
+  fetchUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    unm = await prefs.getString('username') ?? '';
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +28,14 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
+      body: Center(
+        child: Text('Welcome $unm'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      ]),
     );
   }
 }
